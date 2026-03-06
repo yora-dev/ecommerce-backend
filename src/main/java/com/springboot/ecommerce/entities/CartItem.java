264eae57d75f976ccb601b301bddc1ed9b3d25f1
Yordanos Shanbel <yordanosshanbel28@gmail.com>
2026-03-06 12:07:50 +0300

    add Cart and CartItem entities with relationships to User and Product

---
+++ src/main/java/com/springboot/ecommerce/entities/CartItem.java
@@ -0,0 +1,38 @@
+package com.springboot.ecommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CartItem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "cart_id")
+	private Cart cart;
+
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+
+	@Column(name = "quantity")
+	private Integer quantity;
+
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+}
